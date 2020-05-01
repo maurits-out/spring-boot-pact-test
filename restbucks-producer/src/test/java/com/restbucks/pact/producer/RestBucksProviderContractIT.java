173604d6fcb09808790f7c32e2a6f3ab0a4136c3
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-05-01 21:53:04 +0200

    Added initial IT test for testing the contract.

---
+++ restbucks-producer/src/test/java/com/restbucks/pact/producer/RestBucksProviderContractIT.java
@@ -0,0 +1,25 @@
+package com.restbucks.pact.producer;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+@Provider("RestBucksProvider")
+@PactBroker(host = "localhost", port = "8080")
+public class RestBucksProviderContractIT {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
