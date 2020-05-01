b545d670e4542abc4f58bf20404b5194055214c0
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-05-01 21:34:31 +0200

    Setting up the producer.

---
+++ restbucks-producer/src/main/java/com/restbucks/pact/producer/RestBucksApplication.java
@@ -0,0 +1,12 @@
+package com.restbucks.pact.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBucksApplication.class, args);
+    }
+}
